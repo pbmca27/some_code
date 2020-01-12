bbcfb17fcaedd0ec17b5da3d57a526b167963d86
Pratyay Banerjee <pbmca27@gmail.com>
2020-01-12 05:57:08 +0000

    hello

--- testscript.sh
+++ testscript.sh
@@ -2,5 +2,5 @@
 echo "this script is from github"
 NUMBER=$(($NUMBERONE + $NUMBERTWO))
 echo "$NUMBER"
-echo "My automated build on checkin"
+echo "My automated build on checkin..."
 echo "this was run automagically"
