d7c27da8d752e7ac9b2a3b3e98c98e75e39b430d
Pratyay Banerjee <pbmca27@gmail.com>
2020-01-12 05:43:51 +0000

    hello

--- testscript.sh
+++ testscript.sh
@@ -2,5 +2,5 @@
 echo "this script is from github"
 NUMBER=$(($NUMBERONE + $NUMBERTWO))
 echo "$NUMBER"
-echo "Pratyay's automated build on checkin to SCM"
+echo "My automated build on checkin to SCM"
 echo "this was run automagically"
